8f8c42e8d1a268eb080f54ceffddd3815a120ae0
Silver-T <tony.silvestre@gmail.com>
2019-04-07 00:28:07 +1100

    ... and refresh (working React-Bootstrap)

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import logo from './logo.svg';
+import './App.css';
+import {Navbar} from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {'onMon'}
+          </Navbar.Brand>
+        </Navbar>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
